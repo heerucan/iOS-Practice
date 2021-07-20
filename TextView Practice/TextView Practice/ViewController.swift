a1263cd46d460221287af5baa73dea42edcadc5d
heerucan <iwpow0307@gmail.com>
2021-07-20 18:06:56 +0900

    [add] TextView Practice 프로젝트 추가

---
+++ TextView Practice/TextView Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextView Practice
+//
+//  Created by Thisisme Hi on 2021/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
