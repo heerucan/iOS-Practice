ec735d3d4354024025cebb88bd944557cbc0f32d
heerucan <iwpow0307@gmail.com>
2021-07-21 17:54:51 +0900

    [add] textView 기본 세팅

---
+++ TextView Practice/TextView Practice/Extension/UIColor + Extension.swift
@@ -0,0 +1,114 @@
+//
+//  UIColor + Extension.swift
+//  TextView Practice
+//
+//  Created by Thisisme Hi on 2021/07/20.
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var pink000: UIColor {
+        return UIColor(red: 155.0 / 255.0, green: 39.0 / 255.0, blue: 76.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var pink100: UIColor {
+        return UIColor(red: 234.0 / 255.0, green: 69.0 / 255.0, blue: 121.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var pink200: UIColor {
+        return UIColor(red: 236.0 / 255.0, green: 71.0 / 255.0, blue: 123.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var pink210: UIColor {
+        return UIColor(red: 1.0, green: 72.0 / 255.0, blue: 130.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var white: UIColor {
+        return UIColor(white: 252.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var white10: UIColor {
+        return UIColor(white: 1.0, alpha: 0.1)
+    }
+
+    @nonobjc class var black: UIColor {
+        return UIColor(white: 53.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var black100: UIColor {
+        return UIColor(white: 28.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var black200: UIColor {
+        return UIColor(white: 47.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var black300: UIColor {
+        return UIColor(white: 47.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray50: UIColor {
+        return UIColor(white: 92.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var gray100: UIColor {
+        return UIColor(white: 94.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var gray200: UIColor {
+        return UIColor(white: 115.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var gray300: UIColor {
+        return UIColor(white: 133.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray310: UIColor {
+        return UIColor(white: 165.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var gray400: UIColor {
+        return UIColor(white: 183.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var gray500: UIColor {
+        return UIColor(white: 231.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var red100: UIColor {
+        return UIColor(red: 234.0 / 255.0, green: 69.0 / 255.0, blue: 69.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var blue100: UIColor {
+        return UIColor(red: 0.0, green: 120.0 / 255.0, blue: 212.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var logo100: UIColor {
+        return UIColor(red: 181.0 / 255.0, green: 24.0 / 255.0, blue: 73.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var bordergrey: UIColor {
+        return UIColor(red: 96.0 / 255.0, green: 96.0 / 255.0, blue: 96.0 / 255.0, alpha: 0.7)
+    }
+    
+    @nonobjc class var black25: UIColor {
+       return UIColor(white: 0.0, alpha: 0.25)
+    }
+    
+    @nonobjc class var back100: UIColor {
+        return UIColor(red: 31.0 / 255.0, green: 190.0 / 255.0, blue: 200.0 / 255.0, alpha: 1)
+    }
+    
+    @nonobjc class var back200: UIColor {
+        return UIColor(red: 116.0 / 255.0, green: 189.0 / 255.0, blue: 224.0 / 255.0, alpha: 1)
+    }
+    
+    @nonobjc class var back300: UIColor {
+        return UIColor(red: 253.0 / 255.0, green: 175.0 / 255.0, blue: 25.0 / 255.0, alpha: 1)
+    }
+    
+    @nonobjc class var back400: UIColor {
+        return UIColor(red: 250.0 / 255.0, green: 220.0 / 255.0, blue: 70.0 / 255.0, alpha: 1)
+    }
+}
--- TextView Practice/TextView Practice/ViewController.swift
+++ TextView Practice/TextView Practice/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Then
+import SnapKit
 
+class ViewController: UIViewController {
+    // MARK: - Properties
+    let textView = UITextView().then {
+        $0.font = .systemFont(ofSize: 14)
+        $0.layer.cornerRadius = 18
+        $0.backgroundColor = .black200
+        $0.textContainerInset = UIEdgeInsets(top: 18, left: 18, bottom: 18, right: 18)
+    }
+    
+    let letterNumLabel = UILabel().then {
+        $0.text = "0/150"
+        $0.font = .systemFont(ofSize: 16)
+        $0.textColor = .gray200
+        $0.textAlignment = .right
+    }
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configUI()
+        setupAutoLayout()
     }
+    
+    // MARK: - Custom Method
+    func configUI() {
+        view.backgroundColor = .black
+    }
+    
+    func setupAutoLayout() {
+        view.addSubview(textView)
+        view.addSubview(letterNumLabel)
 
-
+        textView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.height.equalTo(190)
+            make.width.equalTo(319)
+        }
+        
+        letterNumLabel.snp.makeConstraints { make in
+            make.top.equalTo(textView.snp.bottom).offset(6)
+            make.trailing.equalToSuperview().inset(28)
+        }
+    }
 }
 
+extension ViewController: UITextViewDelegate {
+    
+}
