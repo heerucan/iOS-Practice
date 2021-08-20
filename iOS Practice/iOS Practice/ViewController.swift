36384ce9b3d66a00ba8295f3e91cc41e6d4e5948
heerucan <iwpow0307@gmail.com>
2021-08-20 14:39:41 +0900

    [init] iOS Practice 프로젝트 세팅

---
+++ iOS Practice/iOS Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Practice
+//
+//  Created by Thisisme Hi on 2021/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
