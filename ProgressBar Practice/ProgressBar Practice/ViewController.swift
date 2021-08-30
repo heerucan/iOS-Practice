d21099ff8ab2dec890852bf4cead8503de69760b
heerucan <iwpow0307@gmail.com>
2021-08-30 18:49:17 +0900

    [init] Progress Bar 초기 세팅

---
+++ ProgressBar Practice/ProgressBar Practice/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ProgressBar Practice
+//
+//  Created by Thisisme Hi on 2021/08/30.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class ViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        setupAutoLayout()
+    }
+    
+    // MARK: - Custom Method
+    func configUI() {
+        
+    }
+    
+    func setupAutoLayout() {
+        
+    }
+}
+
