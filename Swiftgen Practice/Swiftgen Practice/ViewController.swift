8d11c4304775d3effdde882343cf92cf9310bb1d
heerucan <iwpow0307@gmail.com>
2021-11-27 02:55:58 +0900

    [Feat] Font swiftgen 적용

--- Swiftgen Practice/Swiftgen Practice/ViewController.swift
+++ Swiftgen Practice/Swiftgen Practice/ViewController.swift
@@ -14,9 +14,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = Asset.Colors.catchmePink.color
         label.textColor = Asset.Colors.btobBlue.color
         imageView.image = Asset.Assets.icBall.image
+        label.font = FontFamily.NeoDunggeunmo.regular.font(size: 23)
     }
 
 
