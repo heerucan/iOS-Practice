b0535c9f332fe71a1eecbc383d166a0240d8d6a5
heerucan <iwpow0307@gmail.com>
2021-11-27 00:54:35 +0900

    [Add] Swiftgen 프로젝트 추가

---
+++ Swiftgen Practice/Swiftgen Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftgen Practice
+//
+//  Created by Thisisme Hi on 2021/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
