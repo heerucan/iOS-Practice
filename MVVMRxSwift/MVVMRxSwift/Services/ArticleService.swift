d2df3528b4aa7ab196e25a9f450fe4913db82ecf
heerucan <iwpow0307@gmail.com>
2022-04-11 18:16:58 +0900

    [Add] RxSwift로 fetchNews Service 코드

--- MVVMRxSwift/MVVMRxSwift/Services/ArticleService.swift
+++ MVVMRxSwift/MVVMRxSwift/Services/ArticleService.swift
@@ -7,3 +7,47 @@
 
 import Foundation
 import Alamofire
+import RxSwift
+
+class ArticleServcie {
+    
+    func fetchNews() -> Observable<[Article]> {
+        return Observable.create { (observer) -> Disposable in
+            
+            self.fetchNews { (error, articles) in
+                if let error = error {
+                    observer.onError(error)
+                }
+                
+                if let articles = articles {
+                    observer.onNext(articles)
+                }
+                
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+    
+    private func fetchNews(completion: @escaping((Error?, [Article]?) -> Void)) {
+        let urlString = "https://newsapi.org/v2/everything?q=tesla&from=2022-03-11&sortBy=publishedAt&apiKey=96aeb218411746a4a309aafc14fced54#"
+        
+        guard let url = URL(string: urlString) else { return completion(NSError(domain: "에러", code: 404, userInfo: nil), nil)}
+        
+        AF.request(url,
+                   method: .get,
+                   parameters: nil,
+                   encoding: JSONEncoding.default,
+                   headers: nil, interceptor: nil,
+                   requestModifier: nil).responseDecodable(of: ArticleResponse.self) { response in
+            
+            if let error = response.error {
+                return completion(error, nil)
+            }
+            
+            if let articles = response.value?.articles {
+                return completion(nil, articles)
+            }
+        }
+    }
+}
