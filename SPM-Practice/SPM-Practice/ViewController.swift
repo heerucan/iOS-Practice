3cf3f7dd1838f2c334004cf2ae00de817e3918c5
heerucan <iwpow0307@gmail.com>
2022-04-26 03:39:15 +0900

    [Add] SPM 다채로운 연습

--- SPM-Practice/SPM-Practice/ViewController.swift
+++ SPM-Practice/SPM-Practice/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     let myLabel: UILabel = {
         let label = UILabel()
         label.font = WALFont.title1.font
-        label.textColor = .black
+        label.textColor = .orange100
         label.text = "안녕하세요. 김루희입니다. 요를레이"
         return label
     }()
@@ -21,24 +21,60 @@ class ViewController: UIViewController {
     let secondLabel: UILabel = {
         let label = UILabel()
         label.font = WALFont.body6.font
-        label.textColor = .black
+        label.textColor = .gray300
         label.text = "Body6/regular | 16px | 22"
         return label
+    }()
+    
+    let deleteImage: UIImageView = {
+        let image = UIImageView()
+        image.image = WALIcon.icnWarning.image
+        return image
+    }()
+    
+    var navigationBar: WALNavigationBar = {
+        let view = WALNavigationBar(title: "네비게이션바타이틀")
+        view.leftIcon = WALIcon.btnDelete.image
+        return view
+    }()
+    
+    let textField: WALTextField = {
+        let textField = WALTextField()
+        textField.placeholder = "플레이스홀더"
+        textField.isFocusing = true
+        return textField
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(myLabel)
         view.addSubview(secondLabel)
+        view.addSubview(deleteImage)
+        view.addSubview(navigationBar)
+        view.addSubview(textField)
+        
         myLabel.translatesAutoresizingMaskIntoConstraints = false
-        myLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        myLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 400).isActive = true
         myLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
         
         secondLabel.translatesAutoresizingMaskIntoConstraints = false
         secondLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
         secondLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
+        
+        deleteImage.translatesAutoresizingMaskIntoConstraints = false
+        deleteImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 300).isActive = true
+        deleteImage.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
+        deleteImage.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        deleteImage.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        navigationBar.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0).isActive = true
+        navigationBar.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0).isActive = true
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 500).isActive = true
+        textField.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        textField.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
     }
-
-
 }
-
